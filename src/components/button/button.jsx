3026cc1ad8cd7ef213746a09d74045fa53df3f56
ntunga08 <samwelbuluba22@gmail.com>
2025-09-27 16:02:51 +0300

    added button

---
+++ src/components/button/button.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Button = ({
+  children,
+  onClick,
+  className = "",
+  type = "button",
+}) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`bg-blue-500 hover:bg-blue-600 text-white font-semibold px-6 py-2 rounded-lg transition-colors ${className}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
